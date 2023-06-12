b0bbb87d212dae4356fdf73f8f388a2dfdff3fdf
SRG <srg@codeserg.io>
2023-06-12 13:07:25 -0600

    added custom error handling

--- backend/middleware/asyncHandler.js
+++ backend/middleware/asyncHandler.js
@@ -20,6 +20,7 @@
  * allowing for centralized error handling in the application.
  */
 
+// async functions wrapped in this middleware will pass any errors to the Express error handling middleware
 const asyncHandler = (fn) => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch(next);
 }
