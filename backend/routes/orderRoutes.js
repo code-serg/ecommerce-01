1db98cb44428918471575645a585a352fb099092
SRG <srg@codeserg.io>
2023-07-11 11:08:27 -0600

    added orderRoutes, orderController

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { 
+  addOrderItems, 
+  getMyOrders, 
+  getOrderById, 
+  updateOrderToPaid, 
+  updateOrderToDelivered, 
+  getOrders,
+} from '../controllers/orderController.js';
+import { authUser, authAdmin } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+// /api/users - see backend/server.js
+router.route('/').post(addOrderItems).get(authUser, authAdmin, getOrders); // getOrders -> use middleware to enforce admin role
+router.route('/myorders').get(authUser, getMyOrders);
+router.route('/:id').get(authUser, authAdmin, getOrderById);
+router.route('/:id/pay').put(authUser, updateOrderToPaid);
+router.route('/:id/deliver').put(authUser,authAdmin, updateOrderToDelivered);
+
+export default router;
