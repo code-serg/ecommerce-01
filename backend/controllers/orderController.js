5f6ab9f95055e2ba32b0d1200452faf53b5f9e51
SRG <srg@codeserg.io>
2023-07-11 12:39:52 -0600

    partial implementation of the orderController

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,32 @@ import Order from "../models/orderModel.js"
 // @route   POST /api/orders
 // @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send('addOrderItems');
+  const { orderItems, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400); // bad request
+    throw new Error('No order items');
+    return;
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((item) => ({
+        ...item, // name, qty, image, price
+        product: item._id, // product in the order is identified by the product id. Comes from the backend, not the frontend
+        _id: undefined, // TODO - i don't fully understand this and the line above, yet
+      })),
+      user: req.user._id, // logged in user
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
 });
 
 // @desc    Get all orders
@@ -20,14 +45,24 @@ const getOrders = asyncHandler(async (req, res) => {
 // @route   GET /api/orders/myorders
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.send('getMyOrders');
+  const orders = await Order.find({ user: req.user._id }); // logged in user
+
+  res.status(200).json(orders);
 });
 
 // @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send('getOrderById');
+  const order = await Order.findById(req.params.id)
+    .populate('user', 'name email') // also, populate from the user collection - the name and email of the user
+
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404); // not found
+    throw new Error('Order not found');
+  }
 });
 
 // @desc    Update order to paid
