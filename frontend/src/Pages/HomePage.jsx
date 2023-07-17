67580244661c7d583d1fbee2e56433bca04a9d48
SRG <srg@codeserg.io>
2023-07-17 11:07:11 -0600

    added paypal/react package, refactored HomePage and Index for linting errors

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 import Product from '../components/Product';
@@ -7,29 +8,28 @@ import Message from '../components/Message';
 const HomePage = () => {
   const { data: products, isLoading, error } = useGetProductsQuery();
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return (
+      <Message variant="danger">{error.data.message || error.error}</Message>
+    );
+  }
+
   return (
     <>
-      { isLoading ? (
-          <Loader />
-        ) : error ? (
-          <Message variant='danger'> 
-            { error?.data?.message || error.error } 
-          </Message>
-        ) : (
-        <>
-          <h2>Latest Products</h2>
-          <Row>
-            { products.map(product => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3} >
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
-        </>
-        ) 
-      }
+      <h2>Latest Products</h2>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
