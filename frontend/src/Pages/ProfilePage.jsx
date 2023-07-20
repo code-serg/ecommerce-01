6b9ec06ffc6c86123861f394ded05d289ad531eb
SRG <srg@codeserg.io>
2023-07-20 14:21:52 -0600

    ProfilePage now has 'My orders'

--- frontend/src/Pages/ProfilePage.jsx
+++ frontend/src/Pages/ProfilePage.jsx
@@ -5,6 +5,7 @@ import {
   Row,
   Col,
   Form,
+  Table,
   ListGroup,
   Image,
   Card,
@@ -12,10 +13,12 @@ import {
 } from 'react-bootstrap';
 import { toast } from 'react-toastify';
 import { LinkContainer } from 'react-router-bootstrap';
+import { FaTimes } from 'react-icons/fa';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { useProfileMutation } from '../slices/usersApiSlice';
 import { setCredentials } from '../slices/authSlice';
+import { useGetMyOrdersQuery } from '../slices/ordersApiSlice';
 
 const ProfilePage = () => {
   const [name, setName] = useState('');
@@ -31,6 +34,12 @@ const ProfilePage = () => {
   const [updateProfile, { isLoading: loadingUpdateProfile }] =
     useProfileMutation();
 
+  const {
+    data: orders,
+    isLoading: loadingOrders,
+    error: errorOrders,
+  } = useGetMyOrdersQuery();
+
   useEffect(() => {
     if (userInfo) {
       setName(userInfo.name);
@@ -45,6 +54,7 @@ const ProfilePage = () => {
     } else {
       try {
         const res = await updateProfile({
+          _id: userInfo._id,
           name,
           email,
           password,
@@ -112,6 +122,53 @@ const ProfilePage = () => {
       </Col>
       <Col md={9}>
         <h3>My Orders</h3>
+        {loadingOrders && <Loader />}
+
+        {errorOrders ? (
+          <Message variant="danger">{errorOrders?.data?.message}</Message>
+        ) : (
+          <Table striped hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TOTAL</th>
+                <th>PAID</th>
+                <th>DELIVERED</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>{order.totalPrice}</td>
+                  <td>
+                    {order.isPaid ? (
+                      order.paidAt.substring(0, 10)
+                    ) : (
+                      <FaTimes style={{ color: 'red' }} />
+                    )}
+                  </td>
+                  <td>
+                    {order.isDelivered ? (
+                      order.isDelivered.substring(0, 10)
+                    ) : (
+                      <FaTimes style={{ color: 'red' }} />
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/order/${order._id}`}>
+                      <Button variant="light" className="btn-sm">
+                        Details
+                      </Button>
+                    </LinkContainer>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
       </Col>
     </Row>
   );
