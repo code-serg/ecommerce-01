f3e879c9f79a9d8d91fbd85b0b601616d87733fb
SRG <srg@codeserg.io>
2023-07-21 14:40:57 -0600

    eslint

--- frontend/src/Pages/ProductPage.jsx
+++ frontend/src/Pages/ProductPage.jsx
@@ -56,49 +56,36 @@ const ProductPage = () => {
         <Col md={4}>
           <ListGroup variant="flush">
             <ListGroup.Item>
-              {' '}
-              <h3> {product.name} </h3>{' '}
+              <h3> {product.name} </h3>
             </ListGroup.Item>
             <ListGroup.Item>
-              {' '}
               <Rating
                 value={product.rating}
                 text={`${product.numReviews} reviews`}
-              />{' '}
+              />
             </ListGroup.Item>
             <ListGroup.Item> Price: ${product.price} </ListGroup.Item>
-            <ListGroup.Item>
-              {' '}
-              Description: {product.description}{' '}
-            </ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={3}>
           <Card>
             <ListGroup variant="flush">
               <ListGroup.Item>
-                {' '}
                 <Row>
-                  {' '}
-                  <Col> Price: </Col>{' '}
+                  <Col> Price: </Col>
                   <Col>
-                    {' '}
-                    <strong> ${product.price} </strong>{' '}
-                  </Col>{' '}
-                </Row>{' '}
+                    <strong> ${product.price} </strong>
+                  </Col>
+                </Row>
               </ListGroup.Item>
               <ListGroup.Item>
-                {' '}
                 <Row>
-                  {' '}
-                  <Col> Status: </Col>{' '}
+                  <Col> Status: </Col>
                   <Col>
-                    {' '}
-                    {product.countInStock > 0
-                      ? 'In Stock'
-                      : 'Out of Stock'}{' '}
-                  </Col>{' '}
-                </Row>{' '}
+                    {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                  </Col>
+                </Row>
               </ListGroup.Item>
               {product.countInStock > 0 && (
                 <ListGroup.Item>
@@ -140,8 +127,7 @@ const ProductPage = () => {
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
-        {' '}
-        Go Back{' '}
+        Go Back
       </Link>
       {renderProduct()}
     </>
