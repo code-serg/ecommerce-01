a481b3b0d577dad1b77d3e8683f05c4198b7e1c3
SRG <srg@codeserg.io>
2023-07-22 05:31:13 -0600

    display list of orders for the admin OrderListPage

--- frontend/src/Pages/admin/OrderListPage.jsx
+++ frontend/src/Pages/admin/OrderListPage.jsx
@@ -1,5 +1,76 @@
+import { LinkContainer } from 'react-router-bootstrap';
+import { Table, Button } from 'react-bootstrap';
+import { FaTimes } from 'react-icons/fa';
+import Message from '../../components/Message';
+import Loader from '../../components/Loader';
+import { useGetOrdersQuery } from '../../slices/ordersApiSlice';
+
 const OrderListPage = () => {
-  return <div>OrderListPage</div>;
+  const { data: orders, isLoading, error } = useGetOrdersQuery();
+
+  // Helper function to render the orders section
+  const renderOrders = () => {
+    if (isLoading) {
+      return <Loader />;
+    }
+
+    if (error) {
+      return <Message variant="danger">{error}</Message>;
+    }
+
+    return (
+      <Table striped hover responsive className="table-sm">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>CUSTOMER</th>
+            <th>DATE</th>
+            <th>TOTAL</th>
+            <th>PAID</th>
+            <th>DELIVERED</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order) => (
+            <tr key={order._id}>
+              <td>{order._id}</td>
+              <td>{order.user && order.user.name}</td>
+              <td>{order.createdAt.substring(0, 10)}</td>
+              <td>${order.totalPrice}</td>
+              <td>
+                {order.isPaid ? (
+                  order.paidAt.substring(0, 10)
+                ) : (
+                  <FaTimes style={{ color: 'red' }} />
+                )}
+              </td>
+              <td>
+                {order.isDelivered ? (
+                  order.isDelivered.substring(0, 10)
+                ) : (
+                  <FaTimes style={{ color: 'red' }} />
+                )}
+              </td>
+              <td>
+                <LinkContainer to={`/order/${order._id}`}>
+                  <Button variant="primary" className="btn-sm">
+                    Details
+                  </Button>
+                </LinkContainer>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    );
+  };
+
+  return (
+    <>
+      <h3>Orders</h3>
+      {renderOrders()}
+    </>
+  );
 };
 
 export default OrderListPage;
