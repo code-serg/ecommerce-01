2c0d90131aa891e80de12b2b5546006849273fff
SRG <srg@codeserg.io>
2023-08-16 14:34:29 -0600

    setup backend to create product reviews

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,27 @@
 import express from 'express';
-import { getProducts, getProductById, createProduct, updateProduct, deleteProduct } from '../controllers/productController.js';
+import { 
+  getProducts, 
+  getProductById, 
+  createProduct, 
+  updateProduct, 
+  deleteProduct,
+  createProductReview,
+} from '../controllers/productController.js';
 import { authUser, authAdmin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 // /api/products - see backend/server.js
-router.route('/').get(getProducts).post(authUser, authAdmin, createProduct);
-router.route('/:id').get(getProductById).put(authUser, authAdmin, updateProduct).delete(authUser, authAdmin, deleteProduct);
+router
+  .route('/').get(getProducts)
+  .post(authUser, authAdmin, createProduct);
+router
+  .route('/:id')
+  .get(getProductById)
+  .put(authUser, authAdmin, updateProduct)
+  .delete(authUser, authAdmin, deleteProduct);
+router
+  .route('/:id/reviews')
+  .post(authUser, createProductReview);
 
 export default router;
