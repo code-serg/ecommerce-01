618d3a47bf8ef2e162fc0a75f524a778c25e2da1
SRG <srg@codeserg.io>
2023-09-29 17:06:04 -0600

    changed logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import SearchBox from './SearchBox';
 import Message from './Message';
-import logo from '../assets/logo-cs.png';
+// import logo from '../assets/logo-cs.png';
+import logo from '../assets/logo-mc.png';
 import { useLogoutMutation } from '../slices/usersApiSlice';
 import { logout } from '../slices/authSlice';
 import { resetCart } from '../slices/cartSlice';
@@ -28,32 +29,32 @@ const Header = () => {
       navigate('/'); // redirect to home page
     } catch (error) {
       toast.error(
-        <Message variant="danger">
+        <Message variant='danger'>
           Something went wrong. App did not logout user
-        </Message>,
+        </Message>
       );
     }
   };
 
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
-          <LinkContainer to="/">
+          <LinkContainer to='/'>
             <Navbar.Brand>
-              <img src={logo} alt="logo" width="50px" height="50px" />
-              <span className="ms-2">codeSerg - MERN</span>
+              <img src={logo} alt='logo' width='50px' height='50px' />
+              <span className='ms-2'>MERN Commerce</span>
             </Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
               <SearchBox />
-              <LinkContainer to="/cart">
+              <LinkContainer to='/cart'>
                 <Nav.Link>
                   <FaShoppingCart /> Cart
                   {cartItems.length > 0 && (
-                    <Badge pill bg="success" className="ms-1">
+                    <Badge pill bg='success' className='ms-1'>
                       {cartItems.reduce((acc, item) => acc + item.qty, 0)}
                     </Badge>
                   )}
@@ -61,8 +62,8 @@ const Header = () => {
               </LinkContainer>
 
               {userInfo ? (
-                <NavDropdown title={userInfo.name} id="username">
-                  <LinkContainer to="/profile">
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
                     <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
                   <NavDropdown.Item onClick={logoutHandler}>
@@ -70,22 +71,22 @@ const Header = () => {
                   </NavDropdown.Item>
                 </NavDropdown>
               ) : (
-                <LinkContainer to="/login">
-                  <Nav.Link href="/login">
+                <LinkContainer to='/login'>
+                  <Nav.Link href='/login'>
                     <FaUser /> Sign In
                   </Nav.Link>
                 </LinkContainer>
               )}
 
               {userInfo && userInfo.isAdmin && (
-                <NavDropdown title="Admin" id="adminmenu">
-                  <LinkContainer to="/admin/userlist">
+                <NavDropdown title='Admin' id='adminmenu'>
+                  <LinkContainer to='/admin/userlist'>
                     <NavDropdown.Item>Users</NavDropdown.Item>
                   </LinkContainer>
-                  <LinkContainer to="/admin/productlist">
+                  <LinkContainer to='/admin/productlist'>
                     <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
-                  <LinkContainer to="/admin/orderlist">
+                  <LinkContainer to='/admin/orderlist'>
                     <NavDropdown.Item>Orders</NavDropdown.Item>
                   </LinkContainer>
                 </NavDropdown>
